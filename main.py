781dc21fc3704191639b1ce99e4fc41a1dff8d45
lnowarkiewicz <lukasz.nowarkiewicz@gmail.com>
2023-12-04 19:44:31 +0100

    Added escape for toggling fullscreen

--- main.py
+++ main.py
@@ -4,16 +4,22 @@ import json
 from PIL import Image
 import functions
 
+
+
 class App(customtkinter.CTk):
     def __init__(self):
         super().__init__()
 
         self.title(f"cocktailMakerDashboard.py commit-hash: {functions.get_commit_hash(self)}")
         self.geometry("1024x600")
-        # self.bind("<Escape>", self.toggle_fullscreen)
+        self.bind("<Escape>", self.toggle_fullscreen)
         customtkinter.set_appearance_mode("Dark")
         print(functions.print_debug_info(self))
         
+
+    def toggle_fullscreen(self, event=None):
+        is_fullscreen = self.attributes('-fullscreen')
+        self.attributes('-fullscreen', not is_fullscreen)
         
 
 if __name__ == "__main__":
