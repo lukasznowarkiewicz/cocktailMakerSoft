403d5e134fefef27624ec16d303f5768a0c07615
lnowarkiewicz <lukasz.nowarkiewicz@gmail.com>
2023-12-04 19:07:44 +0100

    Created basic main program

---
+++ main.py
@@ -0,0 +1,23 @@
+import customtkinter
+import os
+import json
+from PIL import Image
+import functions
+
+class App(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
+
+        self.title("cocktailMakerDashboard.py")
+        self.geometry("1024x600")
+        # self.bind("<Escape>", self.toggle_fullscreen)
+        customtkinter.set_appearance_mode("Dark")
+        
+        
+
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
+
+
+    
