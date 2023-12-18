e642176f2e2de003566813eb0fce6d7f4512d104
lnowarkiewicz <lukasz.nowarkiewicz@gmail.com>
2023-12-18 15:58:27 +0100

    WIP created sample drinks class

--- main.py
+++ main.py
@@ -9,6 +9,51 @@ import frame_start
 import frame_details
 import frame_preparing
 import frame_settings
+import json
+
+class Drinks:
+    def __init__(self, config_file):
+        self.config_file = config_file
+        self.cocktails = self.load_config()
+
+    def load_config(self):
+        try:
+            with open(self.config_file, 'r') as file:
+                config_data = json.load(file)
+                return config_data
+        except FileNotFoundError:
+            print(f"Error: Config file '{self.config_file}' not found.")
+            return None
+        except json.JSONDecodeError as e:
+            print(f"Error decoding JSON in config file: {e}")
+            return None
+
+    def get_cocktail_names(self):
+        return [cocktail['label'] for cocktail in self.cocktails] if self.cocktails else []
+
+    def get_cocktail_by_name(self, cocktail_name):
+        for cocktail in self.cocktails:
+            if cocktail['label'] == cocktail_name:
+                return cocktail
+        return None
+
+# Example usage:
+config_file_path = 'recipes.json'
+drinks_machine = Drinks(config_file_path)
+
+# Get the names of available cocktails
+cocktail_names = drinks_machine.get_cocktail_names()
+print("Available cocktails:", cocktail_names)
+
+# Get details for a specific cocktail (e.g., Cosmopolitan)
+cosmo_details = drinks_machine.get_cocktail_by_name('Cosmopolitan')
+if cosmo_details:
+    print("\nDetails for Cosmopolitan:")
+    print("Description:", cosmo_details['description'])
+    print("Steps:", cosmo_details['steps'])
+else:
+    print("\nCosmopolitan not found in the configuration.")
+
 
 class App(customtkinter.CTk):
 
