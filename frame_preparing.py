a9c48f8ca289479c40f48accbba16af6cc627a4f
lnowarkiewicz <lukasz.nowarkiewicz@gmail.com>
2023-12-19 22:54:52 +0100

    Updated layout of preparing frame

--- frame_preparing.py
+++ frame_preparing.py
@@ -12,15 +12,28 @@ class Preparing(customtkinter.CTkFrame):
     def __init__(self, parent, controller):
         customtkinter.CTkFrame.__init__(self, parent)
         self.controller = controller
-        label = customtkinter.CTkLabel(self, text="Starting preparation of your drink")
-        label.pack(side="top", fill="x", pady=10)
-        button = customtkinter.CTkButton(self, text="Go to the start page",
+
+        topFrame = customtkinter.CTkFrame(master=self, width=1024, height=600, fg_color="transparent", border_color="white")
+        topFrame.pack(pady=50,padx=50, side="top")
+        self.my_image = customtkinter.CTkImage(light_image=Image.open("cocktail.png"),
+                                  dark_image=Image.open("cocktail.png"),
+                                  size=(300, 300))
+
+        self.image_label = customtkinter.CTkLabel(topFrame, image=self.my_image, text="")  # display image with a CTkLabel
+        self.image_label.pack(side="right", fill="x", pady=10)
+
+        # bottom frame with buttons
+        bottomFrame = customtkinter.CTkFrame(master=self, width=1200, height=200, fg_color="transparent", border_color="white")
+        bottomFrame.pack(pady=50,padx=50, side="bottom")
+        # self.label.con
+        self.button = customtkinter.CTkButton(bottomFrame, text="Return to start screen", width=400, height=60, 
                            command=lambda: controller.show_frame("StartPage"))
-        button.pack(side="top", fill="x", pady=20)
-
-        
-        button2 = customtkinter.CTkButton(self, text="Prepare",
+        # self.button.grid(sticky="nsew", row=1, column=4)
+        self.button.pack(side="left", pady=10)
+        self.button2 = customtkinter.CTkButton(bottomFrame, text="Start",  width=400, height=60, 
                            command=lambda: functions.prepare_drink(self))
-        button2.pack(side="top", fill="x", pady=20)
-
+        self.button2.pack(side="right", pady=10)
+        
+        self.labelSpacer = customtkinter.CTkLabel(bottomFrame, text="            ")
+        self.labelSpacer.pack(side="bottom", fill="x", pady=10)
         
