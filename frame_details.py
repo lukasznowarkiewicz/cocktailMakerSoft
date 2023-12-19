a1aee2c1bcb38b210d0ed73581c8a55c3b74b1ab
lnowarkiewicz <lukasz.nowarkiewicz@gmail.com>
2023-12-19 09:19:12 +0100

    WIP Working on formatting details frame

--- frame_details.py
+++ frame_details.py
@@ -10,16 +10,23 @@ class Details(customtkinter.CTkFrame):
 
     def __init__(self, parent, controller):
         customtkinter.CTkFrame.__init__(self, parent)
+        self.grid_rowconfigure(2, weight=1)  # configure grid system
+        self.grid_columnconfigure(2, weight=1)
         self.controller = controller
         self.label = customtkinter.CTkLabel(self, text="This is page Details")
         self.label.pack(side="top", fill="x", pady=10)
+
+
+        bottomFrame = customtkinter.CTkFrame(master=self, width=1200, height=200, fg_color="transparent", border_color="white")
+        bottomFrame.pack(pady=50,padx=50, side="bottom")
         # self.label.con
-        self.button = customtkinter.CTkButton(self, text="Return to drink choice",
+        self.button = customtkinter.CTkButton(bottomFrame, text="Return to drink choice", width=400, height=60, 
                            command=lambda: controller.show_frame("StartPage"))
-        self.button.pack()
-        button2 = customtkinter.CTkButton(self, text="Make it!",
+        # self.button.grid(sticky="nsew", row=1, column=4)
+        self.button.pack(side="left", pady=10)
+        self.button2 = customtkinter.CTkButton(bottomFrame, text="Make it!",  width=400, height=60, 
                            command=lambda: controller.show_frame("Preparing"))
-        button2.pack()
+        self.button2.pack(side="right", pady=10)
         # self.updateValues(self, "cokolwiek")
     def updateValues(self, drinkName):
         updated_text = f"This is page Details for {drinkName}"
