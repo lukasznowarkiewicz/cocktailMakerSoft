8141983ce7fb80490eefe215d1ade4d0a3c60815
lnowarkiewicz <lukasz.nowarkiewicz@gmail.com>
2023-12-19 08:37:28 +0100

    Done transition between start screen and details with passing arguments

--- frame_start.py
+++ frame_start.py
@@ -13,29 +13,28 @@ class StartPage(customtkinter.CTkFrame):
         customtkinter.CTkFrame.__init__(self, parent)
         self.controller = controller
         self.configure(self, width=1020, height=580, corner_radius=0, fg_color="transparent")
-        # label = customtkinter.CTkLabel(self, text="This is the start page")
-        # label.pack(side="top", fill="x", pady=10)
-        # self.grid(row=0, column=0, sticky="nesw")
         self.grid_rowconfigure(9, weight=10)
         self.grid_columnconfigure(5, weight=1)
 
-        # button2 = customtkinter.CTkButton(self, text="Go to Page Two",
-        #                     command=lambda: controller.show_frame("PageTwo"))
-        
-        # button2.grid(row=0, column=0)
+
 
         # json data import of the recipes
         with open('recipes.json', 'r') as recipes_json:
             recipes_data = json.load(recipes_json)
 
+        button_array = []
+        label = []
         # Button creation and placement with added padding
         for idx, button_data in enumerate(recipes_data):
             # self.image = customtkinter.CTkImage(Image.open(os.path.join(image_path, button_data["image"])), size=(120, 120))
-            label = button_data["label"]
+            label.append(button_data["label"])
+            print(f"Label {label[idx]}")
             #image = button_data["image"]
-            btn = customtkinter.CTkButton(self, text=label, compound="top", font=("arial", 18), border_spacing=10, command=lambda: controller.show_frame("Details", label))
-            btn.grid(row=idx // 5, column=idx % 5, padx=5, pady=5)
-            btn.configure(height = 260, width = 192)
+            # btn = customtkinter.CTkButton(self, text=label, compound="top", font=("arial", 18), border_spacing=10, command=lambda: controller.show_frame("Details", btn._text))
+            button_array.append(customtkinter.CTkButton(self, text=label[idx], compound="top", font=("arial", 18), border_spacing=10, command=lambda l=label[idx]:  controller.show_frame("Details", l)))
+            button_array[idx].grid(row=idx // 5, column=idx % 5, padx=5, pady=5)
+            button_array[idx].configure(height = 260, width = 192)
+            
 
         button3 = customtkinter.CTkButton(self, text="Settings",
                             command=lambda: controller.show_frame("Settings"))
