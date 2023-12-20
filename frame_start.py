998fe7e55c0d03a555d127473b06d38f43e55888
lnowarkiewicz <lukasz.nowarkiewicz@gmail.com>
2023-12-20 22:01:15 +0100

    Updated drinks and added icons to frame start

--- frame_start.py
+++ frame_start.py
@@ -13,8 +13,8 @@ class StartPage(customtkinter.CTkFrame):
         customtkinter.CTkFrame.__init__(self, parent)
         self.controller = controller
         self.configure(self, width=1020, height=580, corner_radius=0, fg_color="transparent")
-        self.grid_rowconfigure(9, weight=10)
-        self.grid_columnconfigure(5, weight=1)
+        # self.grid_rowconfigure(9, weight=10)
+        # self.grid_columnconfigure(6, weight=1)
 
 
 
@@ -26,16 +26,16 @@ class StartPage(customtkinter.CTkFrame):
         label = []
         # Button creation and placement with added padding
         for idx, button_data in enumerate(recipes_data):
-            # self.image = customtkinter.CTkImage(Image.open(os.path.join(image_path, button_data["image"])), size=(120, 120))
+            self.image = customtkinter.CTkImage(Image.open(os.path.join("drinks", button_data["image"])), size=(95, 120))
             label.append(button_data["label"])
             print(f"Label {label[idx]}")
-            image = "drinks/"+button_data["image"]
-            button_array.append(customtkinter.CTkButton(self, image=image, text=label[idx], compound="top", font=("arial", 18), border_spacing=10, command=lambda l=label[idx]:  controller.show_frame("Details", l)))
+            # image = button_data["image"]
+            button_array.append(customtkinter.CTkButton(self, image=self.image, fg_color="transparent", text=label[idx], compound="top", font=("arial", 18), border_spacing=10, command=lambda l=label[idx]:  controller.show_frame("Details", l)))
             button_array[idx].grid(row=idx // 5, column=idx % 5, padx=5, pady=5)
-            button_array[idx].configure(height = 260, width = 192)
+            button_array[idx].configure(height = 180, width = 192)
             
 
-        button3 = customtkinter.CTkButton(self, text="Settings",
+        button3 = customtkinter.CTkButton(self, text="Settings", fg_color="transparent",
                             command=lambda: controller.show_frame("Settings"))
-        button3.grid(sticky="nsew", row=3, column=4)
+        button3.grid(sticky="nsew", row=4, column=4)
         # button3.configure(height=50, width=50)
