1bcce91e4bcff996d23e298e8ee9a833b80cb7c9
lnowarkiewicz <lukasz.nowarkiewicz@gmail.com>
2023-12-31 11:44:07 +0100

    Added buttons in loop on settings frame

--- frame_settings.py
+++ frame_settings.py
@@ -8,6 +8,18 @@ from functools import partial
 
 class Settings(customtkinter.CTkFrame):
 
+    def create_pump_buttons(self, pump_id):
+        on_button = customtkinter.CTkButton(self, text=f"{pump_id} - ON", compound="top", font=("arial", 18), border_spacing=10, command=lambda: functions.controlPump(self, pump_id, "ON"))
+        on_button.pack(side="top", pady=10)
+        off_button = customtkinter.CTkButton(self, text=f"{pump_id} - OFF", compound="top", font=("arial", 18), border_spacing=10, command=lambda: functions.controlPump(self, pump_id, "OFF"))
+        off_button.pack(side="bottom", pady=10)
+
+    def create_all_pump_buttons(self):
+        pump_ids = ["P1", "P2", "P3", "P4", "P5", "P6", "P7", "P8"]
+
+        for pump_id in pump_ids:
+            self.create_pump_buttons(pump_id)
+
     def __init__(self, parent, controller):
         customtkinter.CTkFrame.__init__(self, parent)
         self.controller = controller
@@ -16,9 +28,13 @@ class Settings(customtkinter.CTkFrame):
         button = customtkinter.CTkButton(self, text="Return to start screen",
                            command=lambda: controller.show_frame("StartPage"))
         button.pack()
+        self.create_all_pump_buttons()
+        # onButton = customtkinter.CTkButton(self, text="P1 - ON", compound="top", font=("arial", 18), border_spacing=10, command=lambda: functions.controlPump(self, "P1", "ON") )
+        # onButton.pack(side="left", pady=10)
+        # offButton = customtkinter.CTkButton(self, text="P1 - OFF", compound="top", font=("arial", 18), border_spacing=10, command=lambda: functions.controlPump(self, "P1", "OFF") )
+        # offButton.pack(side="left", pady=10)
 
-        onButton = customtkinter.CTkButton(self, fg_color="blue", text="P1 - ON", compound="top", font=("arial", 18), border_spacing=10, command=lambda: functions.controlPump(self, "P1", "ON") )
-        offButton = customtkinter.CTkButton(self, fg_color="blue", text="P1 - OFF", compound="top", font=("arial", 18), border_spacing=10, command=lambda: functions.controlPump(self, "P1", "OFF") )
-        onButton.pack()
-        offButton.pack()
+
+
+
     
